849ac1f54832332c079d56b54cacea9540604cf7
ktsreddy007 <cloudtech047@gmail.com>
2025-07-11 10:07:56 +0530

    Added app related data

---
+++ SimpleTimeService_app/Controllers/TimeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace SimpleTimeService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTime()
+        {
+            // Use null-conditional operator to avoid null dereference
+            var ip = Request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "Unknown IP";
+            string timeZoneId = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? 
+                    "India Standard Time" : 
+                    "Asia/Kolkata";
+            TimeZoneInfo timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId); 
+            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, timeZoneInfo);
+            var timestamp = localTime.ToString("yyyy-MM-dd HH:mm:ss");
+            var response = new
+            {
+                timestamp,
+                ip
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ SimpleTimeService_app/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.MapGet("/weatherforecast", () =>
+{
+    var summaries = new[] { "Freezing", "Chilly", "Mild", "Warm", "Hot" };
+    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    )).ToArray();
+});
+
+app.MapControllers();
+app.Run("http://0.0.0.0:5000");
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
